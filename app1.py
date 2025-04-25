16fb8a0de5a312505d3ea076cb6cabe88b6cbef7
Mateo Acosta <twishm08@gmail.com>
2025-04-24 21:40:13 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -20,6 +20,16 @@ st.title("Reconocimiento óptico de Caracteres")
 img_file_buffer = st.camera_input("Toma una Foto que contenga texto en ella, este sitio web lo pasará a texto por tí.")
 
 with st.sidebar:
+    st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: lightblue;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
       filtro = st.radio("¿Quieres aplicar filtro?",('Sí quiero', 'No quiero'))
 
 
