bc6e0cd64d1c4f50f4863b0489fd655375523b50
Mateo Acosta <twishm08@gmail.com>
2025-04-24 21:39:35 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,13 +4,23 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: lightblue;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("Reconocimiento óptico de Caracteres")
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.camera_input("Toma una Foto que contenga texto en ella, este sitio web lo pasará a texto por tí.")
 
 with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+      filtro = st.radio("¿Quieres aplicar filtro?",('Sí quiero', 'No quiero'))
 
 
 if img_file_buffer is not None:
@@ -18,7 +28,7 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
     
-    if filtro == 'Con Filtro':
+    if filtro == 'Sí quiero':
          cv2_img=cv2.bitwise_not(cv2_img)
     else:
          cv2_img= cv2_img
